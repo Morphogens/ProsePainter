21eb094bb859511c6ce1d05174b8e49a1be14d13
vipermu <vipermu97@gmail.com>
2021-10-11 19:23:29 +0200

    sanity implemented

--- server/sanity.py
+++ server/sanity.py
@@ -6,39 +6,74 @@ import torchvision
 import numpy as np
 from PIL import Image
 
-from server.vqgan import LayerLoss, LayeredGenerator
-from server.webserver import Layer
+from server.vqgan import Layer, LayeredGenerator
 
-num_optimizations = 100
-lr = 0.5
-target_img_size = 256
+iters_per_mask = 100
+num_optimizations = 1
+lr = 0.3
+target_img_size = (256, 256)
 
-mask1 = np.array(Image.open("server/mask1.png"))
-mask2 = np.array(Image.open("server/mask2.png"))
+cond_img = Image.open('server/cond.png').resize(target_img_size)
+# cond_img = None
+
+style_prompt = " 4k trending on artstation"
+
+background_mask = np.ones(target_img_size + (4, ))
+background_prompt = "Blue sea"
+
+mask_list = [
+    background_mask,
+    np.array(Image.open("server/mask1.png").resize(target_img_size)),
+    np.array(Image.open("server/mask2.png").resize(target_img_size)),
+]
+
+prompt_list = [
+    background_prompt,
+    "Green grass",
+    "A cute dog",
+]
+
+prompt_list = [prompt + style_prompt for prompt in prompt_list]
+
+cum_mask = np.zeros_like(background_mask)
+for mask_idx, mask in enumerate(mask_list[::-1]):
+    inv_mask_idx = len(mask_list) - mask_idx - 1
+    if mask_idx > 0:
+        mask_list[inv_mask_idx] = mask * (1 - cum_mask)
+
+    cum_mask += mask
+    cum_mask = cum_mask.clip(0, 1)
 
 layer_list = []
-for mask in [mask1, mask2]:
+for prompt, mask in zip(prompt_list, mask_list):
     layer = Layer(
+        prompt=prompt,
+        mask=mask,
         color=000,
         strength=1,
-        prompt="a pink cute dog",
-        img=mask,
     )
     layer_list.append(layer)
-
-    break
 
 layered_generator = LayeredGenerator(
     layer_list,
     target_img_size=target_img_size,
     lr=lr,
+    cond_img=cond_img,
 )
 
-for step in range(num_optimizations):
-    rec_img, loss_dict, _state = layered_generator.optimize()
+for optim_step in range(num_optimizations):
+    img_rec, loss_dict, _state = layered_generator.optimize(
+        iters_per_mask=iters_per_mask, )
 
-    for key, value in loss_dict.items():
-        print(key, "loss -->", value)
+    # dirty_rec_img = torchvision.transforms.ToPILImage(mode='RGB')(img_rec[0])
+    # layered_generator.cond_img = dirty_rec_img
+    # layered_generator.reset_gen_latent()
 
-    rec_img_pil = torchvision.transforms.ToPILImage(mode='RGB')(rec_img[0])
-    rec_img_pil.save(f"{step}.jpg")
+# for step in range(num_optimizations):
+#     img_rec, loss_dict, _state = layered_generator.optimize(iters_per_mask=1, )
+
+#     for key, value in loss_dict.items():
+#         print(key, "loss -->", value)
+
+# rec_img_pil = torchvision.transforms.ToPILImage(mode='RGB')(rec_img[0])
+# rec_img_pil.save(f"{step}.jpg")
--- server/vqgan.py
+++ server/vqgan.py
@@ -2,100 +2,102 @@ import time
 import logging
 
 import torch
+import torchvision
 import numpy as np
 from PIL import Image
-from bigotis.models import TamingDecoder
-from PIL import Image
+from bigotis.models import TamingDecoder, Aphantasia
 
-# import webserver
+torch.manual_seed(123)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print("DEVICE: ", device)
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("DEVICE: ", DEVICE)
 
 taming_decoder = TamingDecoder()
-model = taming_decoder.to(device)
+taming_decoder.eval()
+model = taming_decoder.to(DEVICE)
+
+# aphantasia = Aphantasia()
+# aphantasia.eval()
+# model = aphantasia
 
 
-class LayerLoss:
+class Layer:
     def __init__(
         self,
-        layer,
+        prompt: str,
+        mask: np.ndarray,
+        **kwargs,
     ):
-        self.text_emb = taming_decoder.get_clip_text_encodings(layer.prompt, )
-        self.text_emb = self.text_emb.detach()
-        self.text_emb = self.text_emb.to(device)
+        text_latents = model.get_clip_text_encodings(prompt, )
+        text_latents = text_latents.detach()
+        text_latents = text_latents.to(DEVICE)
+        self.text_latents = text_latents
 
         # get alpha mask
         # XXX: getting the first channel to create the mask, not the last one
-        mask = torch.from_numpy(layer.img[:, :, 0]).to(device)
+        mask = torch.from_numpy(mask[:, :, 0]).to(DEVICE)
+        mask = mask.detach()
         mask[mask > 0] = 1
         mask = mask.float()
-        self.mask = mask
+        self.mask = mask[None]
 
-    def __call__(
-        self,
-        image,
-    ):
-        N, C, H, W = image.shape
-        mask = torch.nn.functional.interpolate(
-            self.mask[None, None],
-            (H, W),
-            mode="bilinear",
-        )
-        # merged = image * mask  #+ image.detach() * (1-mask)
-        merged = image
-        self.merged = merged
-        import torchvision
-        torchvision.transforms.ToPILImage(mode="L")(mask[0]).save("mask.jpg")
-        torchvision.transforms.ToPILImage(mode="RGB")(
-            merged[0]).save("masked.jpg")
-        cutouts = taming_decoder.augment(merged, )
-        image_emb = taming_decoder.get_clip_img_encodings(cutouts, )
-        image_emb = image_emb.to(device)
-
-        # dist = image_emb.sub(
-        #     self.text_emb, ).norm(dim=2).div(2).arcsin().pow(2).mul(2)
-
-        dist = -10 * torch.cosine_similarity(self.text_emb, image_emb)
-
-        return dist.mean()
+        return
 
 
 class LayeredGenerator(torch.nn.Module):
     def __init__(
         self,
         layer_list,
-        target_img_size=128,
+        target_img_size=(128, 128),
         lr: float = 0.5,
+        init_latents: torch.Tensor = None,
+        cond_img: torch.Tensor = None,
     ):
         super(LayeredGenerator, self).__init__()
 
-        self.lr = lr
+        self.layer_list = layer_list
         self.target_img_size = target_img_size
-
-        self.layer_loss_list = None
-        self.reset_layers(layer_list, )
+        self.lr = lr
+        self.init_latents = init_latents
+        self.cond_img = cond_img
 
         self.gen_latents = None
+        self.init_img = None
         self.reset_gen_latent()
 
         self.optimizer = None
         self.reset_optimizer()
 
-    def reset_layers(
-        self,
-        layer_list,
-    ):
-        self.layer_loss_list = [LayerLoss(layer) for layer in layer_list]
+        self.num_iters = 0
 
     def reset_gen_latent(self, ):
-        self.gen_latents = taming_decoder.get_random_latents(
-            target_img_height=self.target_img_size,
-            target_img_width=self.target_img_size,
-        )
-        self.gen_latents = self.gen_latents.to(device)
-        self.gen_latents.requires_grad = True
-        self.gen_latents = torch.nn.Parameter(self.gen_latents)
+        if self.gen_latents is None:
+            self.gen_latents = model.get_random_latents(
+                target_img_height=self.target_img_size[0],
+                target_img_width=self.target_img_size[1],
+            )
+            self.gen_latents = self.gen_latents.to(DEVICE)
+            self.gen_latents.requires_grad = True
+            self.gen_latents = torch.nn.Parameter(self.gen_latents)
+
+        else:
+            self.gen_latents.data = model.get_random_latents(
+                target_img_height=self.target_img_size[0],
+                target_img_width=self.target_img_size[1],
+            ).to(DEVICE)
+
+        if self.init_latents is not None:
+            self.gen_latents.data = self.init_latents
+
+        if self.cond_img is not None:
+            self.cond_img = self.cond_img.resize(self.target_img_size)
+            self.gen_latents.data = model.get_latents_from_img(
+                self.cond_img, ).detach()
+
+        self.init_img = model.get_img_from_latents(
+            self.gen_latents).clone().detach()
+
+        self.init_gen_latents = self.gen_latents.clone().detach()
 
     def reset_optimizer(self, ):
         self.optimizer = torch.optim.AdamW(
@@ -105,71 +107,124 @@ class LayeredGenerator(torch.nn.Module):
             weight_decay=0.1,
         )
 
-    def optimize(self, ):
-        try:
-            x_rec = taming_decoder.get_img_from_latents(self.gen_latents)
+    def optimize(
+        self,
+        iters_per_mask: int = 1,
+    ):
+        # try:
+        loss_dict = {}
+        for layer_idx, layer in enumerate(self.layer_list):
+            logging.info(f"\nCOMPUTING LOSS OF LAYER {layer_idx}")
 
-            loss = 0
-            loss_dict = {}
-            for layer_idx, layer_loss in enumerate(self.layer_loss_list):
-                logging.info(f"COMPUTING LOSS OF LAYER {layer_idx}")
+            mask = layer.mask.clone()
+            mask = torch.nn.functional.interpolate(
+                mask[None],
+                self.target_img_size,
+                mode="bilinear",
+            )
 
-                def scale_grad(grad):
-                    print("GRAD SHAPE", grad.shape)
-                    N, C, H, W = grad.shape
-                    mask = layer_loss.mask.clone()
-                    # for covering in self.layer_loss_list[layer_idx]:
-                    #     mask -= covering.mask
-                    #     mask.clamp_(0, 1)
+            # if global_mask is None:
+            #     global_mask = mask
+            # else:
+            #     mask = mask * (1 - global_mask)
+            #     global_mask = (global_mask + mask).clip(0, 1)
 
-                    mask = torch.nn.functional.interpolate(
-                        mask[None, None],
-                        (H, W),
+            for step in range(iters_per_mask):
+                loss = 0
+
+                img_rec = model.get_img_from_latents(self.gen_latents)
+
+                # merged = img_rec * mask
+                merged = img_rec
+
+                torchvision.transforms.ToPILImage(mode='RGB')(img_rec[0]).save(
+                    f"{self.num_iters}-{layer_idx}-{step}.jpg")
+                torchvision.transforms.ToPILImage(mode="L")(
+                    mask[0]).save("mask.jpg")
+                torchvision.transforms.ToPILImage(mode="RGB")(
+                    merged[0]).save("masked.jpg")
+                torchvision.transforms.ToPILImage(mode="RGB")(
+                    self.init_img[0]).save("init_img.jpg")
+
+                img_rec_aug = model.augment(merged, )
+                img_latents = model.get_clip_img_encodings(
+                    img_rec_aug, ).to(DEVICE)
+
+                clip_loss = (layer.text_latents - img_latents).norm(
+                    dim=-1).div(2).arcsin().pow(2).mul(2).mean()
+
+                # clip_loss = -10 * torch.cosine_similarity(
+                #     layer.text_latents,
+                #     img_latents,
+                # ).mean()
+
+                # rand_img_reg = 10 * torch.nn.functional.mse_loss(
+                #     img_rec * (1 - mask),
+                #     self.init_img * (1 - mask),
+                # )
+
+                rand_img_reg = torch.nn.functional.mse_loss(
+                    img_rec * (1 - mask),
+                    self.init_img * (1 - mask),
+                )
+
+                layer_loss = clip_loss + rand_img_reg
+
+                loss += layer_loss
+
+                logging.info(f"RAND REG --> {rand_img_reg}")
+                logging.info(f"CLIP LOSS --> {clip_loss}")
+                logging.info(f"LAYER LOSS --> {layer_loss}")
+                logging.info(f"LOSS --> {loss} \n\n")
+
+                def scale_grad(grad, ):
+                    grad_size = grad.shape[2:4]
+
+                    grad_mask = torch.nn.functional.interpolate(
+                        mask,
+                        grad_size,
+                        mode="bilinear",
                     )
 
-                    masked_grad = grad * mask
+                    if len(grad.shape) == 5:
+                        grad_mask = grad_mask[..., None]
+
+                    masked_grad = grad * grad_mask
 
                     return masked_grad
 
-                loss = layer_loss(x_rec, )
-
-                hook = self.gen_latents.register_hook(scale_grad)
-                hook_img = layer_loss.merged.register_hook(scale_grad)
+                # hook = self.gen_latents.register_hook(scale_grad)
+                hook_img = merged.register_hook(scale_grad)
 
                 self.optimizer.zero_grad()
                 loss.backward(retain_graph=False, )
                 self.optimizer.step()
 
-                hook.remove()
+                # hook.remove()
                 hook_img.remove()
 
-                logging.info(f"LOSS {loss}")
                 loss_dict[f"layer_{layer_idx}"] = loss
 
-        except Exception as e:
-            logging.info(f"XXX: ERROR IN GENERATE {e}")
+                with torch.no_grad():
+                    latent_mask = torch.nn.functional.interpolate(
+                        mask,
+                        self.gen_latents.shape[2:4],
+                        mode="bilinear",
+                    )
+
+                    if len(self.gen_latents.shape) == 5:
+                        latent_mask = latent_mask[..., None]
+
+                    self.gen_latents.data = self.gen_latents.data * latent_mask + self.init_gen_latents * (
+                        1 - latent_mask)
+
+                self.num_iters += 1
+
+            self.init_img = img_rec.detach()
+            self.init_gen_latents = self.gen_latents.detach().clone()
+
+        # except Exception as e:
+        #     logging.info(f"XXX: ERROR IN GENERATE {e}")
 
         state = None
-        return x_rec, loss_dict, state
-
-
-if __name__ == "__main__":
-    from dataclasses import dataclass
-
-    @dataclass
-    class Layer:
-        color: str
-        strength: int
-        prompt: str
-        img: np.ndarray
-
-    layer = Layer(
-        color=000,
-        strength=1,
-        prompt="a pink dog",
-        img=np.zeros((100, 100, 3)),
-    )
-    layer_list = [layer]
-
-    layered_generator = LayeredGenerator(layer_list, )
-    out = layered_generator()
+        return img_rec, loss_dict, state
