079454c6a03218a779945ee1cf58ac9b85f00c84
Joel Simon <joelsimon6@gmail.com>
2021-11-04 12:55:38 -0400

    Use the reusable DrawCanvas for both the mask and the canvas, reafactor into different states and have the options to accept, discard, pause and resume the optimization

--- client/src/drawing/DrawCanvas.svelte
+++ client/src/drawing/DrawCanvas.svelte
@@ -2,16 +2,18 @@
 <script lang="ts">
     import { onMount } from "svelte"
     import { drawLines } from './drawUtils'
-    import { imageContour } from '../imageContour'
+    // import { imageContour } from '../imageContour'
     import { loadImage } from "@/utils";
     
+    export let id: string
     export let width: number
     export let height: number
-    export let radius = 3
+    export let radius = 20
     export let opacity = 3
     export let softness = 3
     export let erasing = false
     export let canvasBase64 = ''
+    export let strokeColor = '#000000'
 
     let canvas: HTMLCanvasElement
     let ctx: CanvasRenderingContext2D
@@ -19,8 +21,8 @@
     let currentStrokeCanvas: HTMLCanvasElement
     let currentStrokeCtx: CanvasRenderingContext2D
     
-    let outlinesCanvas: HTMLCanvasElement
-    let outlinesCtx: CanvasRenderingContext2D
+    // let outlinesCanvas: HTMLCanvasElement
+    // let outlinesCtx: CanvasRenderingContext2D
 
     let mouseDown = false
     let mouseover = false
@@ -31,7 +33,7 @@
         const base64 = canvas.toDataURL()
         // history.push(get(canvasBase64))
         canvasBase64 = base64
-        window.localStorage.setItem('canvasBase64', base64)
+        // window.localStorage.setItem('canvasBase64', base64)
     }
 
     export function clear() {
@@ -41,35 +43,36 @@
     
     export function set(src:CanvasImageSource) {
         ctx.drawImage(src, 0, 0)
+        canvasChanged()
     }
 
     onMount(async () => {
         ctx = canvas.getContext('2d')
-        outlinesCtx = outlinesCanvas.getContext('2d')
+        // outlinesCtx = outlinesCanvas.getContext('2d')
         currentStrokeCtx = currentStrokeCanvas.getContext('2d')
         // drawContext.set({ canvas, ctx })
         // Draw the start image that was saved
-        const last = window.localStorage.getItem('canvasBase64')
+        const last = null//window.localStorage.getItem('canvasBase64')
         if (last) {
             ctx.drawImage(await loadImage(last), 0, 0)
             // canvasBase64.set(last)
             canvasBase64 = last
-            drawContours(imageContour(canvas, ctx) as number[][][])
+            // drawContours(imageContour(canvas, ctx) as number[][][])
         }
     })
     
-    function drawContours(contours: number[][][]) {
-        outlinesCtx.clearRect(0, 0, width, height)
-        outlinesCtx.lineWidth = 1
-        outlinesCtx.setLineDash([3, 3]);
-        for (const poly of contours) {
-            drawLines( outlinesCtx, poly)
-        }
-    }
+    // function drawContours(contours: number[][][]) {
+    //     outlinesCtx.clearRect(0, 0, width, height)
+    //     outlinesCtx.lineWidth = 1
+    //     outlinesCtx.setLineDash([3, 3]);
+    //     for (const poly of contours) {
+    //         drawLines( outlinesCtx, poly)
+    //     }
+    // }
     // Listen for changes to the canvas and update contours.
-    $: if (canvasBase64.length && ctx) {
-        drawContours(imageContour(canvas, ctx) as number[][][])
-    }
+    // $: if (canvasBase64.length && ctx) {
+    //     drawContours(imageContour(canvas, ctx) as number[][][])
+    // }
 
     function strokeDone() {
         if (!mouseDown){
@@ -77,6 +80,7 @@
         }
         mouseDown = false
         currentStroke = []
+        ctx.globalCompositeOperation = 'source-over'
         ctx.drawImage(currentStrokeCanvas, 0, 0)
         currentStrokeCtx.clearRect(0, 0, width, height)
         canvasChanged()
@@ -98,7 +102,7 @@
                 ctx.globalCompositeOperation = 'destination-over'
                 currentStrokeCtx.globalAlpha = 1 - opacity;
                 currentStrokeCtx.filter = `blur(${softness}px)`;
-                currentStrokeCtx.strokeStyle = 'black'
+                currentStrokeCtx.strokeStyle = strokeColor
                 currentStrokeCtx.lineWidth = radius
                 currentStrokeCtx.lineCap = 'round'
                 currentStrokeCtx.lineJoin = 'round'
@@ -109,7 +113,7 @@
     }
 
 </script>
-<div id='canvasesContainer'
+<div id={id} class='canvasesContainer'
     on:mousedown={() => (mouseDown = true)}
     on:mouseup={() => strokeDone()}
     on:mouseleave={() => strokeDone()}
@@ -124,15 +128,15 @@
         bind:this={canvas}
         {width}
         {height}
-        style='opacity:{mouseover ? 1 : 0}'
     />
-    <canvas
+        <!-- style='opacity:{mouseover ? 1 : 0}' -->
+    <!-- <canvas
         id='outlinesCanvas'
         bind:this={outlinesCanvas}
         {width}
         {height}
         style='opacity:{mouseover ? 0 : 1}'
-    />
+    /> -->
     <canvas
         id="currentStrokeCanvas"
         bind:this={currentStrokeCanvas}
@@ -143,11 +147,11 @@
 
 
 <style>
-    #canvasesContainer {
+    .canvasesContainer {
         position: relative;
         cursor: crosshair;
     }
-    #currentStrokeCanvas, #outlinesCanvas {
+    #currentStrokeCanvas  {
         position: absolute;
         top: 0px;
         left: 0px;
