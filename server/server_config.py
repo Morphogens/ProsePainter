cba4fc20a38e5a1e483707fbddabe4e36ee3492a
vipermu <vipermu97@gmail.com>
2021-12-03 12:43:53 +0100

    ch: set "ViT-B/32", as default

--- server/server_config.py
+++ server/server_config.py
@@ -7,8 +7,8 @@ DEBUG = True
 DEBUG_OUT_DIR = "./generations"
 
 CLIP_MODEL_NAME_LIST = [
-    # "ViT-B/32",
-    "ViT-B/16",
+    "ViT-B/32",
+    # "ViT-B/16",
     # "RN50x16",
     # "RN50x4",
 ]
